9f015b0db28c84b15361b798a0ec6ead684b67a0
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-01-21 21:25:29 -0300

    refactor(routes): aplicando lazyload nos componentes

--- react-project/src/routes.js
+++ react-project/src/routes.js
@@ -2,27 +2,26 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import Route from './components/Route';
-import Login from './pages/Login';
-import Trailers from './pages/Trailers';
 
-// import Loading from './components/Loading';
+import Loading from './components/Loading';
 
-// const longLazyImportPage = (pageName) =>
-//   React.lazy(() =>
-//     Promise.all([
-//       import(`./pages/${pageName}`),
-//       new Promise((resolve) => setTimeout(resolve, 3000)),
-//     ]).then(([module]) => module),
-//   );
+const longLazyImportPage = (pageName) =>
+  React.lazy(() =>
+    Promise.all([
+      import(`./pages/${pageName}`),
+      new Promise((resolve) => setTimeout(resolve, 3000)),
+    ]).then(([module]) => module),
+  );
 
-// const Login = longLazyImportPage('Login');
+const Login = longLazyImportPage('Login');
+const Trailers = longLazyImportPage('Trailers');
 
 const Routes = () => (
   <Switch>
-    {/* <React.Suspense fallback={<Loading />}> */}
-    <Route path="/" exact component={Login} />
-    <Route path="/trailers" component={Trailers} isPrivate />
-    {/* </React.Suspense> */}
+    <React.Suspense fallback={<Loading />}>
+      <Route path="/" exact component={Login} />
+      <Route path="/trailers" component={Trailers} isPrivate />
+    </React.Suspense>
   </Switch>
 );
 
