498c6f597fa10115ef2f7d8b1df011b55e28ef8d
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-01-21 21:22:08 -0300

    refactor(Layout): aplicando fade in no conteudo da pagina

--- react-project/src/components/Layout/styles.js
+++ react-project/src/components/Layout/styles.js
@@ -1,6 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import PageContainer from '../PageContainer';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 export const Container = styled(PageContainer)`
   display: flex;
@@ -11,6 +20,8 @@ export const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  animation: ${fadeIn} 2s ease backwards;
+  animation-delay: 0.5s;
 
   background: ${({ theme }) => theme.contentBackground};
 `;
