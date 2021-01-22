eaf07d7e407bf9cf3892c78b00c9c1e96b9912a0
GustavoBlaze <gustavoblaze1524@gmail.com>
2021-01-21 21:13:18 -0300

    refactor(Login): aplicando fade in no formulario e na logo

--- react-project/src/pages/Login/styles.js
+++ react-project/src/pages/Login/styles.js
@@ -1,9 +1,18 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import PageContainer from '../../components/PageContainer';
 import LogoWithTheme from '../../components/LogoWithTheme';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
 
 export const Container = styled(PageContainer)`
   display: flex;
@@ -16,6 +25,7 @@ export const Logo = styled(LogoWithTheme)`
   width: 90%;
   max-width: 274px;
   margin-bottom: 51px;
+  animation: ${fadeIn} 1.5s ease backwards;
 `;
 
 export const Form = styled.form`
@@ -24,6 +34,8 @@ export const Form = styled.form`
   width: 90%;
   max-width: 275px;
   align-items: stretch;
+  animation: ${fadeIn} 2s ease backwards;
+  animation-delay: 0.5s;
 `;
 
 export const EmailInput = styled(Input)`
